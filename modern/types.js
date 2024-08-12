4c3b6e6182ba265a5d7c28c3e432de2a0a745e70
Alex Lee <iasandcb@gmail.com>
2024-08-12 11:51:45 +0900

    Review types

--- modern/types.js
+++ modern/types.js
@@ -1,4 +1,19 @@
-const user = Symbol();
-console.log(user);
-const n = BigInt('1');
-console.log(n);
+const n = 1; // number
+console.log('number', n);
+const s = 'Hello World' // string
+console.log('string', s);
+const b = true; // boolean
+console.log('boolean', b);
+const m = null;
+console.log('null', m);
+const p = undefined;
+console.log('undefined', p);
+const a = [1, 2, 3]; // array
+console.log('array', a);
+let a1 = a[1]; // index 1
+console.log('array index 1', a1);
+a[1] = 4; 
+console.log('array index 1 again', a1);
+console.log('array index 1 directly', a[1]);
+const o = {name: 'Alex Lee', age: 1, smoking: false};
+console.log('object', o);
